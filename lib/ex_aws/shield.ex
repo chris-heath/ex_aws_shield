ccc8e12702a23b827454669857fc5cde257d7afc
Justin Tang <justin.tang@vonage.com>
2021-10-08 10:51:55 -0400

    adding list-protections

--- lib/ex_aws/shield.ex
+++ lib/ex_aws/shield.ex
@@ -3,16 +3,47 @@ defmodule ExAws.Shield do
   Documentation for `ExAws.Shield`.
   """
 
-  @doc """
-  Hello world.
+  import ExAws.Utils
 
-  ## Examples
+  @version "2016-06-16"
 
-      iex> ExAws.Shield.hello()
-      :world
+  @type list_protections_opts :: [
+    next_token: binary,
+    max_results: integer
+  ]
+  @spec list_protections() :: ExAws.Operation.JSON.t()
+  @spec list_protections(opts :: list_protections_opts) :: ExAws.Operation.JSON.t()
+  def list_protections(opts \\ []) do
+    query_params =
+      opts
+      |> normalize_opts()
 
-  """
-  def hello do
-    :world
+    request(:list_protections, query_params)
+  end
+
+  ####################
+  # Helper Functions #
+  ####################
+  defp request(action, data) do
+    operation = action |> Atom.to_string() |> Macro.camelize()
+
+    ExAws.Operation.JSON.new(
+      :shield,
+      %{
+        data: data,
+        headers: [
+          {"x-amz-target", "AWSShield_#{format_version()}.#{operation}"},
+          {"content-type", "application/x-amz-json-1.1"}
+        ]
+      }
+    )
+  end
+
+  defp format_version(), do: String.replace(@version, "-", "")
+
+  defp normalize_opts(opts) do
+    opts
+    |> Enum.into(%{})
+    |> camelize_keys
   end
 end
